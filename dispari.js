28b0766de1c051ca792cab6967faab1fe8ff5dea
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-08-22 16:20:08 +0200

    Added word from user

--- dispari.js
+++ dispari.js
@@ -1,1 +1,15 @@
 console.log('JS OK')
+
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// 1. Chiediamo all'utente una parola
+const userWord = prompt('Dammi una parola')
+
+// Funzione:
+// 1. Come la chiamo?
+// 2. ho bisogno di paramentri?
+// 3. ho bisogno di restituire un valore?
+// 4. Se si, di che tipo?
+
