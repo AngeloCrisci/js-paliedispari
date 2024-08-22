159c3c94fb059cd9989869fb5e479898b1a02393
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-08-22 16:44:18 +0200

    Secondo esercizio (Added information from user)

--- dispari.js
+++ dispari.js
@@ -1,1 +1,9 @@
 console.log('JS OK')
+
+// Versione non BONUS
+
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+
+const userNumber = prompt('Dammi un numero da 1 a 5')
+const userChoice = prompt('Pari o Dispari')
+
